55193497d146fc9f53bd6eb1f3bc9d03451487ce
WaseurGIT <waseurrahman31@gmail.com>
2026-04-02 20:37:24 +0600

    origin updated

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 // middlewares
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://online-academyd.onrender.com"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   }),
